de11f66f2082af109b32123d5cfa2997d2f275e4
SaverioCascelli <saverio.cascelli@gmail.com>
2023-01-26 15:56:42 +0100

    [add] axios get json from api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import axios from 'axios';
+
+export default{
+  name:'App',
+  data(){
+    return{
+      url: 'http://127.0.0.1:8000/api/projects',
+      projects:[],
+   }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.url)
+        .then(result =>{
+          this.projects = result.data;
+          console.log(result.data);
+        })
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
+
+}
+
+
 </script>
 
 <template>
-  vue 
-  <HelloWorld />
+  
+
 </template>
 
 <style scoped lang="scss">
-@use './style/general.scss'
+@use './style/general.scss';
 </style>
