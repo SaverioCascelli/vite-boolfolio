6806dd06ef0464383ec4937bf6d97669a0045eb4
SaverioCascelli <saverio.cascelli@gmail.com>
2023-01-26 16:35:43 +0100

    [add] project card for dinamic print of api mil5

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script>
-
+import projectCard from './components/projectCard.vue';
 import axios from 'axios';
 
+
 export default{
+  components: { projectCard },
   name:'App',
   data(){
     return{
@@ -14,8 +16,9 @@ export default{
     getApi(){
       axios.get(this.url)
         .then(result =>{
-          this.projects = result.data;
-          console.log(result.data);
+          this.projects = result.data.data;
+
+          console.log(this.projects);
         })
     }
   },
@@ -29,10 +32,28 @@ export default{
 </script>
 
 <template>
-  
+  <main>
+
+    <div class="project-container">
+      <projectCard
+      v-for="project in projects"
+      :key="project.id"
+      :name="project.name"
+      :text="project.summary"
+      />
+    </div>
+
+  </main>
 
 </template>
 
 <style scoped lang="scss">
 @use './style/general.scss';
+.project-container{
+  width: calc(100%);
+  margin: 25px;
+  display: flex;
+  flex-wrap: wrap;
+  }
+
 </style>
---
+++ src/components/projectCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name:'projectCard',
+    props:{
+        name:String,
+        text:String,
+    }
+}
+</script>
+
+
+<template>
+    <div class="project-card"> 
+        <div class="card-title">
+            <strong>Title:{{ name }}</strong>
+        </div>
+
+        <div class="card-text">
+            {{ text }}
+        </div>
+    </div>
+  
+</template>
+
+
+<style>
+.project-card{
+    width: calc(100% / 6);
+    margin: 10px;
+}
+</style>
